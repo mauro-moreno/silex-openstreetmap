2c543d0ff1422eeee91bf08a98eaa8824d14cbb0
Mauro Moreno <moreno.mauro.emanuel@gmail.com>
2016-02-11 04:19:13 +0000

    Basic service provider

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# silex-openstreetmap
+# Silex OpenStreetMap
+
 Silex Wrapper for the OpenStreetMap API
+
+## Installation
+
+Require the latest version of Scientist Laravel using [Composer](https://getcomposer.org/).
+
+    composer require mauro-moreno/silex-openstreetmap
+
+Enable Service Provider
+
+```php
+<?php
+$app = new Silex\Application;
+$app->register(new MauroMoreno\OpenStreetMap\Silex\OpenStreetMapServiceProvider);
+```
+
+## Usage
+
+```php
+<?php
+$app['openstreetmap.nominatim']->reverse(40.748817, -73.985428);
+```
---
+++ src/OpenStreetMapServiceProvider.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: magia
+ * Date: 2/11/16
+ * Time: 1:02 AM
+ */
+
+namespace MauroMoreno\OpenStreetMap\Silex;
+
+use MauroMoreno\OpenStreetMap\Client\NominatimClient;
+use Silex\Application;
+use Silex\ServiceProviderInterface;
+
+class OpenStreetMapServiceProvider implements ServiceProviderInterface
+{
+
+    /**
+     * {@inheritdoc}
+     */
+    public function register(Application $app)
+    {
+        $app['openstreetmap.format'] =
+            isset($app['openstreetmap.format']) ?
+                $app['openstreetmap.format'] : 'json';
+        $app['openstreetmap.nominatim.addressinfo'] =
+            isset($app['openstreetmap.nominatim.addressinfo']) ?
+                $app['openstreetmap.nominatim.addressinfo'] : 1;
+
+        $app['openstreetmap.nominatim'] = $app->protect(function () use ($app) {
+            return new NominatimClient([
+                'format' => $app['openstreetmap.format'],
+                'addressinfo' => $app['openstreetmap.nominatim.addressinfo']
+            ]);
+        });
+    }
+    /**
+     * {@inheritdoc}
+     */
+    public function boot(Application $app)
+    {
+    }
+
+}
